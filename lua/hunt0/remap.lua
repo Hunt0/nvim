93154c40330b694d64ebfd41407fe252e84d2b58
Brandon <bdh2991@gmail.com>
2024-08-22 09:50:51 -0400

    Fix keymaps

--- lua/hunt0/remap.lua
+++ lua/hunt0/remap.lua
@@ -1,10 +1,10 @@
 vim.keymap.set("i", "kj", "<Esc>", { noremap = true })
 vim.keymap.set("n", "<localleader>f", ":b<SPACE>")
-vim.keymap.set("n", "<localleader>b", ":b#")
+vim.keymap.set("n", "<localleader>b", "<cmd>b#<cr>")
 vim.keymap.set("n", "<localleader>c", ":!")
 vim.keymap.set("n", "<localleader>j", "o<Esc>")
 vim.keymap.set("n", "<localleader>k", "O<Esc>")
-vim.keymap.set("n", "<localleader>cwd", ":cd %:p:h")
+vim.keymap.set("n", "<localleader>cwd", "<cmd>cd %:p:h<cr>")
 
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { noremap = true })
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { noremap = true })
