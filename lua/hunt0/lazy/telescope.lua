684b01a9081277c39849d6f8d209295f52f036b9
Brandon <bdh2991@gmail.com>
2024-08-23 13:11:13 -0400

    live grep keymap

--- lua/hunt0/lazy/telescope.lua
+++ lua/hunt0/lazy/telescope.lua
@@ -8,5 +8,6 @@ return {
         require('telescope').setup({})
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+        vim.keymap.set('n', '<leader>fw', builtin.live_grep, {})
     end
 }
