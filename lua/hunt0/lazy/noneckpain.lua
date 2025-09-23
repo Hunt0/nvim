ba0410efa5511813bc744229e2888197bcf09eda
Brandon Hunt <brandon.hunt@wec.net>
2025-09-23 13:04:36 -0400

    Add no neck pain plugin

---
+++ lua/hunt0/lazy/noneckpain.lua
@@ -0,0 +1,13 @@
+return {
+    "shortcuts/no-neck-pain.nvim",
+    config = function()
+        require("no-neck-pain").setup({
+            width = 120,
+            autocmds = {
+                enableOnVimEnter = true,
+                enableOnTabEnter = true,
+                disableOnVimLeave = true,
+            }
+        })
+    end
+}
